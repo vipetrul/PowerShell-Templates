b54439d341752611c2393b2510c13fdd5a19e255
Valerij Petrulevich <vipetrul@gmail.com>
2016-04-13 21:24:12 -0500

    Job with Limited scope

---
+++ JobWithLimitedScope.ps1
@@ -0,0 +1,25 @@
+﻿Set-ExecutionPolicy Unrestricted -Scope Process
+
+$functions = {
+    function b {"Hello B"};
+    function C {"Hello C"};
+}
+
+$jobDefinition = {
+    param ($functions, $other)
+
+    Write-Output $another;
+}
+$PID;
+Start-Job -ScriptBlock { 
+$PID;
+$ExecutionContext.SessionState.Applications.Clear();
+$ExecutionContext.SessionState.Scripts.Clear();
+$ExecutionContext.SessionState.LanguageMode = "NoLanguage";
+Get-Variable | foreach { $_.Visibility = "private" };
+Get-Command | foreach { $_.Visibility = "private" };
+. ([scriptblock]::Create($using:functions));
+
+b;
+c;
+}  | Wait-Job | Receive-Job
